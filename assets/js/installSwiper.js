4bf61b61d7eacbe0878f9945c44a90e08fac522a
Xasan0 <xasanceek0733@gmail.com>
2024-03-01 05:29:26 +0500

    ready project added new page

---
+++ assets/js/installSwiper.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.swiper', {
+
+  direction: 'horizontal',
+  loop: true,
+  speed: 500,
+  spaceBetween: 0,
+
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  autoplay: {
+    delay: 2000,
+  },
+
+});
